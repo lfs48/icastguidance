5aadf72bfe36b9867f21eee203ae9a7f713a1c91
lucas schraier <lucasfschraier@gmail.com>
2019-07-23 19:24:32 -0400

    Add session route

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,13 @@ const express = require("express");
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const users = require("./routes/api/users");
+const session = require("./routes/api/session");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/api/users", users);
+app.use("/api/session", session);
 
 const db = require('./config/keys').mongoURI;
 mongoose
