7fcf3c0c20c8c6f2618ad953cf3dec0e5634bcdd
lucas schraier <lucasfschraier@gmail.com>
2019-07-23 18:53:45 -0400

    Add users route

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const users = require("./routes/api/users");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use("/api/users", users);
 
 const db = require('./config/keys').mongoURI;
 mongoose
