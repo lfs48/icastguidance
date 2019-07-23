c6e8965b425101361da512c6338643d17f74cef8
lucas schraier <lucasfschraier@gmail.com>
2019-07-23 17:35:08 -0400

    Use body-parser middleware

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 const db = require('./config/keys').mongoURI;
 mongoose
   .connect(db, { useNewUrlParser: true })
