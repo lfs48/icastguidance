964a72c69c79a8daaba3ce6beb73118c1592ef77
lucas schraier <lucasfschraier@gmail.com>
2019-07-23 20:28:59 -0400

    Use jsonwebtoken to generate session token upon registration/login

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
 const router = express.Router();
 
@@ -17,7 +19,18 @@ router.post('/', (req, res) => {
         bcrypt.compare(password, user.password_digest)
           .then(isMatch => {
             if (isMatch) {
-              res.json({msg: 'Success'});
+                const payload = {id: user.id, name: user.name};
+                jwt.sign(
+                    payload,
+                    keys.jwtKey,
+                    {expiresIn: '7d'},
+                    (err, token) => {
+                        res.json({
+                            success: true,
+                            token: 'Bearer ' + token
+                        });
+                    }
+                );
             } else {
               return res.status(400).json({password: 'Incorrect password'});
             }
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
 const router = express.Router();
 
@@ -11,8 +13,7 @@ router.post('/', (req, res) => {
             return res.status(422).json({error: "Username already registered"}) 
         } else {
             const newUser = new User({
-                username: req.body.username,
-                session_token: Math.random().toString(36).substr(2, 9)
+                username: req.body.username
             })
 
             bcrypt.genSalt(10, (err, salt) => {
@@ -20,7 +21,16 @@ router.post('/', (req, res) => {
                     if (err) throw err;
                     newUser.password_digest = hash;
                     newUser.save()
-                    .then(user => res.json(user))
+                    .then(user => {
+                        const payload = { id: user.id, username: user.username };
+          
+                        jwt.sign(payload, keys.jwtKey, { expiresIn: '7d' }, (err, token) => {
+                          res.json({
+                            success: true,
+                            token: "Bearer " + token
+                          });
+                        });
+                      })
                     .catch(err => console.log(err));
                 })
             })
