1ffe4c03f3717fabebbbe8a7b1a2b85b27716b43
Lucas <lucasfschraier@gmail.com>
2019-07-29 21:23:03 -0400

    Render navbar and splash components, wrap in routers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import {Switch, Route, HashRouter} from 'react-router-dom';
+import Navbar from './components/navbar/navbar';
+import Splash from './components/splash/splash';
 
 function App() {
   return (
-    <header>icastguidance</header>
+    <HashRouter>
+      <main id="app-container">
+        <Navbar/>
+        <Switch>
+          <Route path="/" component={Splash}></Route>
+        </Switch>
+      </main>
+    </HashRouter>
   );
 }
 
