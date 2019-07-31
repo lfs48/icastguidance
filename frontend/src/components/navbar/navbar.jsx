bb5b346103d708ca33901ac8d4b183d5624bfe2c
Lucas <lucasfschraier@gmail.com>
2019-07-30 22:59:14 -0400

    Render buttons with container divs

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -2,6 +2,20 @@ import React from 'react';
 
 export default function Navbar() {
     return(
-        <></>
+        <div id="navbar-container">
+            <div id="navbar-left-container">
+                <button>Monster Creator</button>
+                <button>Damage Calculator</button>
+            </div>
+
+            <div id="navbar-right-container">
+                <span>Welcome</span>
+                <button>Register</button>
+                <button>Sign In</button>
+            </div>
+        </div>
+        
+
+    
     );
 };
