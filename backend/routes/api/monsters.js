60cbb9ff578033225741fd60f8ee235f350cd81d
lucas schraier <lucasfschraier@gmail.com>
2019-09-29 00:31:12 -0400

    Add post route

---
+++ backend/routes/api/monsters.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const Monster = require('../../models/Monster');
+const keys = require('../../config/keys');
+
+const router = express.Router();
+
+router.post('/', (req, res) => {
+    const monster = new Monster({
+        name: req.body.name,
+        author: req.body.author,
+        cr: req.body.cr,
+        type: req.body.type,
+        content: req.body.content
+    });
+    monster.save()
+    .then(monster => {
+        res.json({
+            success: true,
+            data: monster
+        });
+    });
+});
+
+module.exports = router;
