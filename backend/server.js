5c477a02672dcd0053e3543debce0da49d88bbb1
lucas schraier <lucasfschraier@gmail.com>
2019-09-29 00:30:39 -0400

    Add monsters route

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,14 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const users = require("./routes/api/users");
 const session = require("./routes/api/session");
+const monsters = require("./routes/api/monsters");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/api/users", users);
 app.use("/api/session", session);
+app.use("/api/monsters", monsters);
 app.use(express.static(__dirname + '/frontend'));
 
 const db = require('./config/keys').mongoURI;
