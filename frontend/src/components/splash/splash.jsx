97707da60ad70d0f200e44521561bd7c3b227ccd
lucas schraier <lucasfschraier@gmail.com>
2019-09-29 00:00:04 -0400

    Render container div and monster creator component

--- frontend/src/components/splash/splash.jsx
+++ frontend/src/components/splash/splash.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import MonsterForm from '../monster_creator/monster_form';
 
 export default function Splash() {
     return(
-        <></>
+        <div id="splash-container-div">
+            <MonsterForm/>
+        </div>
     );
 };
