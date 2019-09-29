a5cf80cef1e99f7877f2f5c3aa237beb261fcba8
lucas schraier <lucasfschraier@gmail.com>
2019-09-28 22:22:14 -0400

    Render inputs for name, CR, AC, HP

--- frontend/src/components/monster_creator/monster_form.jsx
+++ frontend/src/components/monster_creator/monster_form.jsx
@@ -1,7 +1,52 @@
 import React from 'react';
 
-export default function MonsterForm {
+export default function MonsterForm() {
+    const options = Array(30).fill().map((_, i) => <option value={`${i+1}`}>{i+1}</option>);
     return(
-        <></>
+        <div id="monster-form-container">
+            <form id="monster-form">
+                <label
+                    for="monster-name-input"
+                >
+                    Name
+                </label>
+                <input
+                    id="monster-name-input"
+                    type="text"
+                ></input>
+                <label
+                    for="monster-CR-input"
+                >
+                    CR
+                </label>
+                <select
+                    id="monster-CR-input"
+                >
+                    <option value="0">0</option>
+                    <option value="1/8">1/8</option>
+                    <option value="1/4">1/4</option>
+                    <option value="1/2">1/2</option>
+                    {options}
+                </select>
+                <label
+                    for="monster-AC-input"
+                >
+                    AC
+                </label>
+                <input
+                    id="monster-AC-input"
+                    type="text"
+                ></input>
+                <label
+                    for="monster-HP-input"
+                >
+                    HP
+                </label>
+                <input
+                    id="monster-HP-input"
+                    type="text"
+                ></input>
+            </form>
+        </div>
     );
 }
