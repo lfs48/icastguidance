2607b6af27b54da5ca8255d0e671052dc46b8fed
lucas schraier <lucasfschraier@gmail.com>
2019-10-01 15:56:55 -0400

    Rename entities folder to api

---
+++ frontend/src/util/api/user_api_util.js
@@ -0,0 +1,23 @@
+import $ from 'jquery';
+
+export const fetchUser = (id) => {
+    return $.ajax({
+        method: 'GET',
+        url: `api/users/${id}`
+    });
+};
+
+export const fetchUsers = () => {
+    return $.ajax({
+        method: 'GET',
+        url: 'api/users'
+    });
+};
+
+export const createUser = (user) => {
+    return $.ajax({
+        method: 'POST',
+        url: 'api/users',
+        data: { user: user }
+    });
+};
