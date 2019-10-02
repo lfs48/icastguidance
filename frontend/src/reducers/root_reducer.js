c8338c4e8b69f7cbe6178fe005bc09fa15192f8a
lucas schraier <lucasfschraier@gmail.com>
2019-10-02 15:15:31 -0400

    Include ui slice of state

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -3,7 +3,8 @@ import entitiesReducer from './entities/entities_reducer';
 import uiReducer from './ui/ui_reducer';
 
 const rootReducer = combineReducers({
-    entities: entitiesReducer
+    entities: entitiesReducer,
+    ui: uiReducer
 });
 
 export default rootReducer;
