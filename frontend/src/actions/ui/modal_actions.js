5b18e96146d18ae3b7e7ec8253fbb16f095d8aa4
lucas schraier <lucasfschraier@gmail.com>
2019-10-02 15:10:32 -0400

    Define modal closing actions

--- frontend/src/actions/ui/modal_actions.js
+++ frontend/src/actions/ui/modal_actions.js
@@ -7,8 +7,16 @@ export const openModal = (component) => ({
     type: actionTypes.OPEN_MODAL
 });
 
+export const closeModal = () => ({
+    type: actionTypes.CLOSE_MODAL
+});
+
 //Thunk actions
 
 export const dispatchOpenModal = (component) => (dispatch) => {
     return (component) => dispatch(openModal(component));
+}
+
+export const dispatchCloseModal = () => (dispatch) => {
+    return () => dispatch(closeModal());
 }
