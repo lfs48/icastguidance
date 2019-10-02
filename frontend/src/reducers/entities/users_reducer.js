6c59806af8ad2325f729742c3e7ec82028dc98bb
lucas schraier <lucasfschraier@gmail.com>
2019-10-02 19:57:15 -0400

    Handle RECEIVE_USER action

--- frontend/src/reducers/entities/users_reducer.js
+++ frontend/src/reducers/entities/users_reducer.js
@@ -1,4 +1,7 @@
 import { merge } from 'lodash';
+import * as actionTypes from '../../actions/types';
+import { RECEIVE_USER } from '../../actions/entities/user_actions';
+import entitiesReducer from '../ui/ui_reducer';
 
 const usersReducer = (state = {}, action) => {
     const newState = merge({}, state);
@@ -6,6 +9,11 @@ const usersReducer = (state = {}, action) => {
 
     switch(action.type){
         default: return state;
+        
+        case(RECEIVE_USER): {
+            newState[action.user.id] = action.user;
+            return newState;
+        }
     }
 };
 
