b2655472b792142aab25154d3d2ecd46ce0ac879
lucas schraier <lucasfschraier@gmail.com>
2019-10-02 16:57:00 -0400

    Render modal component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import {Switch, Route, HashRouter} from 'react-router-dom';
 import Navbar from './components/navbar/navbar';
 import Splash from './components/splash/splash';
+import Modal from './components/modal/modal';
 import { Provider } from 'react-redux';
 
 function App({store}) {
   return (
     <Provider store={store}>
       <HashRouter>
+        <Modal/>
         <main id="app-container">
           <Navbar/>
           <Switch>
