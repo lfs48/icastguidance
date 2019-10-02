a5d1f9e97d1c89c1249451bc9364f13bf9bb51e2
lucas schraier <lucasfschraier@gmail.com>
2019-10-02 15:14:27 -0400

    Initial commit

---
+++ frontend/src/actions/entities/user_actions.js
@@ -0,0 +1,43 @@
+import * as UsersAPIUtil from '../../util/api/user_api_util';
+
+export const RECEIVE_USER = "RECEIVE_USER";
+export const RECEIVE_ALL_USERS = "RECEIVE_ALL_USERS";
+export const RECEIVE_SIGNUP_ERRORS = "RECEIVE_SIGNUP_ERRORS";
+
+// Standard actions
+
+const receiveUser = (user) => ({
+    type: RECEIVE_USER,
+    user: user
+});
+
+const receiveAllUsers = (users) => ({
+    type: RECEIVE_ALL_USERS,
+    users: users
+});
+
+const receiveSignupErrors = (errors) => ({
+    type: RECEIVE_SIGNUP_ERRORS,
+    errors: errors
+});
+
+// Thunk actions
+
+export const fetchUser = (id) => (dispatch) => {
+    return UsersAPIUtil.fetchUser(id).then(
+        (user) => dispatch( receiveUser(user) )
+    );
+};
+
+export const fetchUsers = () => (dispatch) => {
+    return UsersAPIUtil.fetchUsers().then(
+        (users) => dispatch(receiveAllUsers(users))
+    );
+};
+
+export const createUser = (formUser) => (dispatch) => {
+    return UsersAPIUtil.createUser(formUser).then(
+        (user) => dispatch(receiveUser(user)),
+        (errors) => dispatch(receiveSignupErrors(errors.responseJSON))
+    );
+};
