729115e0e4af5ec01f4d48a6be8db0903206e8e9
lucas schraier <lucasfschraier@gmail.com>
2019-10-03 19:30:01 -0400

    Define entities action types

--- frontend/src/actions/types.js
+++ frontend/src/actions/types.js
@@ -1,3 +1,15 @@
+// entities action types
+
+    // User action types
+
+    export const RECEIVE_USER = "RECEIVE_USER";
+    export const RECEIVE_ALL_USERS = "RECEIVE_ALL_USERS";
+    export const RECEIVE_SIGNUP_ERRORS = "RECEIVE_SIGNUP_ERRORS";
+
+    // Monster action types
+
+    export const RECEIVE_MONSTER = "RECEIVE_MONSTER";
+
 // ui action types
 
     // Modal action types
