40f65f80b9a76f598ffb5e285746d36682faecb7
lucas schraier <lucasfschraier@gmail.com>
2019-10-03 19:27:11 -0400

    Include monsters slice of state

--- frontend/src/reducers/entities/entities_reducer.js
+++ frontend/src/reducers/entities/entities_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import usersReducer from './users_reducer';
+import monstersReducer from './monsters_reducer';
 
 const entitiesReducer = combineReducers({
-    users: usersReducer
+    users: usersReducer,
+    monsters: monstersReducer
 });
 
 export default entitiesReducer;
